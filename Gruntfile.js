654e9d06d1957d75e2b44b5f42c8787c01e1e410
hellerve <veit.heller@student.htw-berlin.de>
2015-05-05 13:58:01 +0200

    Changed port number

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function (grunt) {
         connect: {
             server: {
                 options: {
-                    port: 8000,
+                    port: 8001,
                     base: './'
                 }
             }
