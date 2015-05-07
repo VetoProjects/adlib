17ca93428c6e890fe433dd17cb0fae8d4d610673
hellerve <veit.heller@student.htw-berlin.de>
2015-05-07 16:53:21 +0200

    Added kernel to html

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@
   _canvasCtx.clearRect(0, 0, _canvas.width, _canvas.height);
 
   var _width = _canvas.width,
-  var _height = _canvas.height;
+      _height = _canvas.height;
 
   _canvasCtx.fillStyle = 'rgb(100, 100, 100)';
   _canvasCtx.fillRect(0, 0, _width, _height);
